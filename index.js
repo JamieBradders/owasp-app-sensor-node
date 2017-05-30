c749011f6dd94188913c13fb81ac81693730b801
Jamie Bradley <jamie@endeavour-digital.com>
2017-05-30 09:34:51 +0100

    added code mapping

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 class AppSensor {
   constructor(options) {
-    this.config = options ? options : require('./module.config.js')
+    this.config = options ? options : require('./config/sensor.config.js')
+    this.codes = require('./config/sensor.codes.json')
   }
 
-  log() {
-    console.log('something is happening', this.config)
+  /**
+   * log()
+   * @param {string} code
+   * @param {string} [message]
+   * Submits a log the chosen external logging system.
+   * Logs the message to the terminal window [might be removed].
+   */
+  log(code, message) {
+    const payload = this.codes[code]
+
+    if (message) {
+      payload.customMessage = message
+    }
+
+    console.log('this is payload', payload)
+
+    return payload
   }
 }
 
